6780e0fdf0ad7888cf348d8e23eae6ad11bdacc7
Alejandro Rojas <kmachappy@pm.me>
2022-05-25 19:53:04 -0700

    added styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Header(){
-    return <h1>this is the header</h1>
+    return(
+        <nav className='nav'>
+                {/* // {'/'} this just evaluates to a string so it doesn't matter if '/' or {'/ '} they are both strings regardless */}
+            <Link to={'/'}>
+                <div>People App</div>
+            </Link>
+        </nav>
+    )
+
 }
